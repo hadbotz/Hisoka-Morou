e5e8b60ec89e546d8f059fd65e677e6abb2ebbef
HAD <103582402+hadbotz@users.noreply.github.com>
2022-04-22 22:22:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By HAD STORE.
+   * Contact Me on wa.me/6285749433476
+   * Subscribe https://www.youtube.com/channel/UCuezNL34NQm_AcGce5lHBWg/videos?view=0&sort=p
 */
 
 const fs = require('fs')
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6285749433476']
+global.premium = ['6285749433476']
+global.packname = 'HAD STORE'
+global.author = 'HAD BOTZZ'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
